01361a608644c8b3a0b8b9a3e8c1878f586088da
Karam Midani <karambassmid@hotmail.com>
2025-06-03 22:24:01 -0400

    handling onSelect using DOM, just before starting state and using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import {CORE_CONCEPTS} from "./data.js";
 import {Header} from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton";
 
+function handleSelect(selectedButton){
+    switch(selectedButton){
+        case "Components":
+            document.getElementById("DynamicContent").innerHTML = "Components";
+            break;
+        case "JSX":
+            document.getElementById("DynamicContent").innerHTML = "JSX";
+            break;
+        case "Props":
+            document.getElementById("DynamicContent").innerHTML = "Props";
+            break;
+        case "State":
+            document.getElementById("DynamicContent").innerHTML = "State";
+            break;
+    }
+}
 function App() {
   return (
     <div>
@@ -15,6 +32,16 @@ function App() {
                   <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
                   <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
               </ul>
+          </section>
+          <section id="examples">
+              <h2>Examples</h2>
+              <menu>
+                  <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+                  <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+                  <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+                  <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+              </menu>
+              <p id="DynamicContent">Please select an example</p>
           </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,3 @@
+export default function TabButton(props) {
+    return <li><button onClick={props.onSelect}>{props.children}</button></li>;
+}
